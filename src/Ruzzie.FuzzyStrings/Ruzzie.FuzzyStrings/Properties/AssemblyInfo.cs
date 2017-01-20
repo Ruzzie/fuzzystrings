3996e6ed162f01e22afea834503080062bce0a1a
Dorus Verhoeckx <dorus.verhoeckx@gmail.com>
2017-01-20 12:05:19 +0100

    - cleaned up assemblyinfo

--- src/Ruzzie.FuzzyStrings/Ruzzie.FuzzyStrings/Properties/AssemblyInfo.cs
+++ src/Ruzzie.FuzzyStrings/Ruzzie.FuzzyStrings/Properties/AssemblyInfo.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -14,15 +13,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-#if !PORTABLE
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("7b155d6c-3f96-41f9-bbac-b8f70e5000de")]
-#endif
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
